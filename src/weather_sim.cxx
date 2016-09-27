d9fc960cb41e5a3379699266c9ee723e015280e2
Boris Egorov <egorov@linux.com>
2016-09-27 14:07:59 +0700

    weather_sim: Manual wrapping for two interfaces example

--- src/weather_sim.cxx
+++ src/weather_sim.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 
 void adjust_values(double *alpha1, double *beta1, double *alpha2, double *beta2)
 {
@@ -42,8 +43,30 @@ std::ostream& operator<<(std::ostream& out, const reading& r)
                << "beta: " << r.beta_value({}, {});
 }
 
+std::tuple<double, double, double, double> get_adjusted_values(
+        const reading& r,
+        location,
+        datetime t1,
+        datetime t2)
+{
+    double alpha1 = r.alpha_value({}, t1);
+    double beta1 = r.alpha_value({}, t1);
+
+    double alpha2 = r.alpha_value({}, t2);
+    double beta2 = r.alpha_value({}, t2);
+
+    adjust_values(&alpha1, &beta1, &alpha2, &beta2);
+    return std::make_tuple(alpha1, beta1, alpha2, beta2);
+}
+
+
 int main()
 {
     auto r = reading{2.3, 4.2};
     std::cout << r << '\n';
+    auto t = get_adjusted_values(r, {}, {}, {});
+    std::cout << "alpha: " << std::get<0>(t)
+              << ", beta: " << std::get<1>(t) << '\n';
+    std::cout << "alpha: " << std::get<2>(t)
+              << ", beta: " << std::get<3>(t) << '\n';
 }
